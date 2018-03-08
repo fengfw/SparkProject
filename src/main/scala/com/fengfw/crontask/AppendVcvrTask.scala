7a345491dfaa1370db98cf5d17453560d259a7d6
fengfw <451488920@qq.com>
2018-03-08 09:52:23 +0800

    Update AppendVcvrTask.scala

--- src/main/scala/com/fengfw/crontask/AppendVcvrTask.scala
+++ src/main/scala/com/fengfw/crontask/AppendVcvrTask.scala
@@ -1,6 +1,5 @@
 package com.fengfw.vcvrbid
 
-import java.io.File
 import java.text.SimpleDateFormat
 
 import scala.sys.process._
@@ -25,11 +24,10 @@ object AppendVcvrTask {
 
     val pday=ptime.subSequence(0,8).toString
     val phour=ptime.substring(8,10)
-    val rootPath:File=new File("/user/root/flume/express")
-    val subFile:File=new File(rootPath.getAbsolutePath+File.separator+"pday="+pday
-      +File.separator+"phour="+phour)
+    val rootPath="/user/flume/express"
+    val notMatch="/data/report/vcvr_notmatch"
 
-    var vcvrPath=s"/user/root/flume/express/pday=$pday/phour=$phour/vcvr*.lzo"
+    val vcvrPath = s"/user/flume/express/pday=$pday/phour=$phour/vcvr*.lzo"
     val vcvrRDD=sc.newAPIHadoopFile[LongWritable, Text, TextInputFormat](vcvrPath)
         .repartition(100)
         .map(line=> {
@@ -39,14 +37,12 @@ object AppendVcvrTask {
       (bidUuid, value)
     })
 
-    val impBidRDD=getImpBidRDD(sc,rootPath.getAbsolutePath,pday,phour,hours)
-    val outputPath=s"/data/append_vcvr/pday=$pday/phour=$phour"
+    val impBidRDD=getImpBidRDD(sc,rootPath,pday,phour,hours)
+    val outputPath=s"/data/report/append_vcvr/pday=$pday/phour=$phour"
     val cleanOutputPath = "hdfs dfs -rmr " + outputPath+ "" !
-	//将RDD进行join操作，并按条件匹配合并RDD
-    val unionRDD: Unit =vcvrRDD.join(impBidRDD).map(a =>{
-      val vcvr=a._2._1
-      val bid=a._2._2.split("\3")(0)
-      val vcvrBid=vcvr+"\3"+bid
+
+    vcvrRDD.join(impBidRDD).map(a =>{
+      val vcvrBid=a._2._1+"\3"+a._2._2
       vcvrBid
     })
     .coalesce(20)
@@ -55,21 +51,19 @@ object AppendVcvrTask {
     sc.stop()
   }
 
-  //对多个相同的<key,value>形式的RDD进行合并
   def getImpBidRDD(sc:SparkContext,rootPath:String,pday:String,phour:String,hours:Int): RDD[(String, String)] ={
-    val hour:Int=Integer.valueOf(phour)
     val dateFormat:SimpleDateFormat=new SimpleDateFormat("yyyyMMddHH")
     var time=dateFormat.parse(pday+phour)
-    var impBidPath=rootPath+s"/pday=$pday/phour=$phour/impression_bid*.lzo"
+    val impBidPath = rootPath + s"/pday=$pday/phour=$phour/impression_bid*.lzo"
     val pathList:ArrayBuffer[String]=ArrayBuffer(impBidPath)
     for(i <- 0 to hours){
       val oneHourAgo=dateFormat.format(time.getTime-(60*60*1000))
       val newPday=oneHourAgo.subSequence(0,8)
       val newPhour=oneHourAgo.subSequence(8,10)
       time=dateFormat.parse(oneHourAgo)
-      pathList+=s"/user/root/flume/express/pday=$newPday/phour=$newPhour/impression_bid*.lzo"
+      pathList+=s"/user/flume/express/pday=$newPday/phour=$newPhour/impression_bid*.lzo"
     }
-    var impBidRDD=getFileRDD(sc,impBidPath)
+    val impBidRDD = getFileRDD(sc, pathList(0))
     pathList.remove(0)
     for(path <- pathList){
       impBidRDD.union(getFileRDD(sc,path))
@@ -77,18 +71,26 @@ object AppendVcvrTask {
     impBidRDD
   }
 
-  //对日志文件按行切片，生成<key,value>形式的RDD
   def getFileRDD(sc:SparkContext,rootPath:String): RDD[(String, String)] ={
     val fileRDD=sc.newAPIHadoopFile[LongWritable, Text, TextInputFormat](rootPath)
-      .repartition(100)
-      .map(line=>{
-      val value=line._2.toString
-      val impression:String=value.split("\3",-1)(1)
-      val sign:String=impression.split("\1",-1)(1)
-      val uuid:String=sign.split("\2",-1)(0)
-      (uuid,value)
-    })
+//      .repartition(20) //减少重分区，可提升计算性能
+      .mapPartitions(iterator => {
+        val tmpIterator = new scala.collection.mutable.ListBuffer[(String, String)]()
+        while (iterator.hasNext) {
+          val value=iterator.next().toString
+          val bid:String=value.split("\3",-1)(1)
+          val adslots=bid.split("\1",-1)(7).split("\2",-1)
+          if(adslots.length>1){ //空字段过多，可能导致切割字段数量未达到要求，需判断下所需字段是否在字段组中
+            val adslot_type=adslots(1)
+            if(adslot_type.equals("video")){ //增加过滤条件，减轻RDD数据集
+              val sign:String=bid.split("\1",-1)(1)
+              val uuid=sign.split("\2",-1)(0)
+              tmpIterator.append((uuid, bid))
+            }
+          }
+        }
+        tmpIterator.iterator
+      })
     fileRDD
   }
-
 }
