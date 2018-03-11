c00a3c199220a2249602b85e633f251d70a0c891
fengfw <451488920@qq.com>
2018-03-11 15:29:47 +0800

    update scripts

--- scripts/appendVcvrBid.py
+++ scripts/appendVcvrBid.py
@@ -26,7 +26,16 @@ if __name__ == "__main__":
     target_time = getDate(2)
     hours ='2'
     print("target_time=%s" % (target_time))
-    submit_command='''spark-submit --class com.fengfw.vcvrbid.AppendVcvrTask --master yarn-client --conf spark.io.compression.codec=org.apache.spark.io.LZ4CompressionCodec --conf spark.memory.useLegacyMode=true --conf spark.shuffle.memoryFraction=0.8 --conf spark.storage.memoryFraction=0.1 --conf spark.storage.unrollFraction=0.1 --conf spark.dynamicAllocation.enabled=false --conf spark.network.timeout=1200s --executor-memory 8G --num-executors 80 --conf spark.sql.shuffle.partitions=160 --name SparkRepartitoinBase_source%s --queue normal /home/storm/fuwei.feng/vcvr-bid/target/vcvrbid-1.0-SNAPSHOT.jar %s %s'''% (target_time,target_time,hours)
-    print "run command = %s" % submit_command
-    ret = commands.getoutput(submit_command)
-    print "run command = %s" % ret
+    submit_command1='''spark-submit --class com.fengfw.vcvrbid.AppendVcvrTask --master yarn-client --conf spark.io.compression.codec=org.apache.spark.io.LZ4CompressionCodec --conf spark.memory.useLegacyMode=true --conf spark.shuffle.memoryFraction=0.8 --conf spark.storage.memoryFraction=0.1 --conf spark.storage.unrollFraction=0.1 --conf spark.dynamicAllocation.enabled=false --conf spark.network.timeout=1200s --executor-memory 8G --num-executors 80 --conf spark.sql.shuffle.partitions=160 --name SparkRepartitoinBase_source%s --queue normal /home/storm/fengfw/vcvr-bid/target/vcvrbid-1.0-SNAPSHOT.jar %s %s'''% (target_time,target_time,hours)
+    print "run command appendVCVR = %s" % submit_command1
+    ret1 = commands.getstatusoutput(submit_command1)
+    print "run command appendVCVR = %s" % ret1[1]
+	
+	if ret1[0]==0:
+        print("target_time=%s" % (target_time))
+        submit_command2='''spark-submit --class com.ipinyou.vcvr2kafka.vcvr2kafkaTask --master yarn-client --conf spark.io.compression.codec=org.apache.spark.io.LZ4CompressionCodec --conf spark.memory.useLegacyMode=true --conf spark.shuffle.memoryFraction=0.8 --conf spark.storage.memoryFraction=0.1 --conf spark.storage.unrollFraction=0.1 --conf spark.dynamicAllocation.enabled=false --conf spark.network.timeout=1200s --executor-memory 8G --num-executors 80 --conf spark.sql.shuffle.partitions=160 --name SparkRepartitoinBase_source%s --queue normal --files /home/storm/fengfw/vcvr-bid/conf/vcvr2kafka.conf /home/storm/fengfw/vcvr-bid/target/vcvrbid-1.0-SNAPSHOT.jar %s'''% (target_time,target_time)
+        print "run command vcvr2kafka = %s" % submit_command2
+        ret2 = commands.getoutput(submit_command2)
+        print "run command vcvr2kafka = %s" % ret2
+    else:
+        print "ret1's status is not 0"
