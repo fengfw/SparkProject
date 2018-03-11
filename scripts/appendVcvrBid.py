cf836315fb8082c2707e2d4040d32073b276de75
fengfw <451488920@qq.com>
2018-03-11 15:31:36 +0800

    update scripts

--- scripts/appendVcvrBid.py
+++ scripts/appendVcvrBid.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     ret1 = commands.getstatusoutput(submit_command1)
     print "run command appendVCVR = %s" % ret1[1]
 	
-	if ret1[0]==0:
+    if ret1[0]==0:
         print("target_time=%s" % (target_time))
         submit_command2='''spark-submit --class com.ipinyou.vcvr2kafka.vcvr2kafkaTask --master yarn-client --conf spark.io.compression.codec=org.apache.spark.io.LZ4CompressionCodec --conf spark.memory.useLegacyMode=true --conf spark.shuffle.memoryFraction=0.8 --conf spark.storage.memoryFraction=0.1 --conf spark.storage.unrollFraction=0.1 --conf spark.dynamicAllocation.enabled=false --conf spark.network.timeout=1200s --executor-memory 8G --num-executors 80 --conf spark.sql.shuffle.partitions=160 --name SparkRepartitoinBase_source%s --queue normal --files /home/storm/fengfw/vcvr-bid/conf/vcvr2kafka.conf /home/storm/fengfw/vcvr-bid/target/vcvrbid-1.0-SNAPSHOT.jar %s'''% (target_time,target_time)
         print "run command vcvr2kafka = %s" % submit_command2
